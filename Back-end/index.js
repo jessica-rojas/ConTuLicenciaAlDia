943adbeceed332c0c339375dfb2d47f6bf36c1b3
David Blandon <90638931+davidbb10@users.noreply.github.com>
2021-11-14 10:50:42 -0500

    CAMBIOS BACK

---
+++ Back-end/index.js
@@ -0,0 +1,6 @@
+var app = require("./app");
+var mongoose =require('./ app/conexBD/conn');
+var port = 4000;
+app.listen(port, () => {
+  console.log("servidor corriendo ok");
+});
---
+++ Back-end/src/conexDB/conn.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+mongoose
+.connect("mongodb://localhost:27017/licenciasdb", {
+useNewUrlParser: true,
+useCreateIndex: true,
+useUnifiedTopology: true,
+useFindAndModify: false,
+},(err,res)=>{
+if(err){
+throw err;
+}else{
+console.log('La conexion a la base de datos fue correcta...')
+}
+});
+
+module.exports = conn;
